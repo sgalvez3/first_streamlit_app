5cb44fa3b8e3e7abd54efbe4b6aac171056b5da0
sgalvez3 <110841320+sgalvez3@users.noreply.github.com>
2022-08-08 15:21:31 -0500

    Update streamlit_app.py
    Added Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,5 @@ streamlit.text('🥑🍞Avocado Toast')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+streamlit.title(' Build Your Own Fruit Smoothie ')
 streamlit.dataframe(my_fruit_list)
