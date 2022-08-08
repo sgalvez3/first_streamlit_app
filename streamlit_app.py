91191023266f88196c5e8c6a4493217a466115af
sgalvez3 <110841320+sgalvez3@users.noreply.github.com>
2022-08-08 15:57:19 -0500

    Update streamlit_app.py
    corrected typo in pre-pick

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.title(' Build Your Own Fruit Smoothie ')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index){'Avocado','Straberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)['Avocado','Straberries'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
